e24389cbe96917a5321cb43ed221579fd19fe66c
karoljd <karoljd@stud.ntnu.no>
2020-05-20 18:47:41 +0200

    Mindre endringer

--- customer/Customer_taxi_confirmation.js
+++ customer/Customer_taxi_confirmation.js
@@ -30,7 +30,7 @@ class Customer_taxi_confirmation extends React.Component {
                     </View>
                     {this.state.isReviewed && (
                         <View style={styles.ratingContainer}>
-                            <Rating/>
+                            <Rating {...this.props} />
                         </View>
                     )}
                 </View>
