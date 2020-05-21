55529108f3de50f41a0a81c4a86d0a676b3ecfe4
Kenneth <keennetk@ntnu.no>
2020-05-21 17:55:04 +0200

    Added getOrderPhoneNumber.js Unsure if works correct, as i cant get to driver funskjons at this time

---
+++ driver/getOrderPhoneNumber.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import {getToken} from "../common_files/ourFunctions";
+import {serverIp} from "../common_files/Texts";
+import {updateMobNum} from "../redux/actions";
+import store from '../redux/store'
+
+
+export const getOrderPhoneNumber = async () => {
+    const tokenGotten = await getToken();
+    await fetch(serverIp+ '/takeorder', {
+        method: 'POST',
+        headers: {'content-type': 'application/json'},
+        body: JSON.stringify({
+            orderId: this.props.orderId,
+            deviceId: this.props.deviceId,
+            token: tokenGotten,
+        }),
+    })
+        .then((response) => response.json())
+        .then((json) => {
+            console.log('took Order, got Phone number:')
+            console.log(json[0].phoneNumber)
+            this.props.updateMobNum(json[0].phoneNumber)
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+const mapStateToProps = (state) => ({
+    orderId: state.order_id,
+    deviceId: state.deviceId,
+    mobileNumber: state.mobileNumber,
+})
+
+
+export default getOrderPhoneNumber
